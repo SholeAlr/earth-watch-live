0255ab5d956331266cf069080642fe8fb8313711
Shole Alirezaei <alirezaei.shole@gmail.com>
2026-02-25 18:05:32 +0330

    feat: add markers to the map

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,7 +1,15 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
-const Map = () => {
+import { IconMarker } from "./IconMarker";
+import { useDisasters } from "../../hooks";
+
+const MapView = () => {
+  const { data: events, isLoading, isError } = useDisasters();
+
+  if (isLoading) return <p>Loading disasters...</p>;
+  if (isError) return <p>Error loading disasters!</p>;
+
   return (
     <MapContainer
       center={[20, 0]}
@@ -12,8 +20,20 @@ const Map = () => {
         attribution='&copy; OpenStreetMap contributors'
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
       />
+
+      {events?.map((event) =>
+        event.geometry.map((geo, index) => (
+          <IconMarker
+            key={`${event.id}-${index}`}
+            position={[geo.coordinates[1], geo.coordinates[0]]}
+            category={event.categories[0]?.id || ""}
+            title={event.title}
+            date={new Date(geo.date).toLocaleDateString()}
+          />
+        )),
+      )}
     </MapContainer>
   );
 };
 
-export default Map;
+export default MapView;
